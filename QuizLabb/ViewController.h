2a32c2545c5760eb40e0db605408d2390ec5bcd5
guasc0 <gual.scolari@hotmail.com>
2017-01-17 09:03:10 +0100

    Initial Commit

---
+++ QuizLabb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizLabb
+//
+//  Created by Gualberto Scolari on 2017-01-17.
+//  Copyright © 2017 Gualberto Scolari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuizLabb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuizLabb
+//
+//  Created by Gualberto Scolari on 2017-01-17.
+//  Copyright © 2017 Gualberto Scolari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
